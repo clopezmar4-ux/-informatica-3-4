841a2371b4e47a4766a4a8af3b7fad1d914b3ba5
clopezmar4-ux <clopezmar4@gmail.com>
2026-04-20 18:19:38 +0000

    recipe

--- recipe.c
+++ recipe.c
@@ -2,5 +2,8 @@
 
 int main(void)
 {
-        printf("Coconut Cranberry Dessert \n");
+    printf("Coconut Cranberry Dessert \n");
+    printf("\n");
+    printf("1 1/2 cups\t Graham cracker crumbs (about 24 full-sized graham crackers)\n");
+    print("1/2 cup\t butter)
 }
