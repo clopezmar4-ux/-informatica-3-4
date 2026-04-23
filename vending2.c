49cfbf08bdb87b3525dd4416d5afba5841ab8bde
clopezmar4-ux <clopezmar4@gmail.com>
2026-04-23 15:49:31 +0000

    vending2

--- vending2.c
+++ vending2.c
@@ -18,8 +18,8 @@ int main(void)
     printf("Welcome to the Snack Shop! \n");
     printf("---------------------------\n");
     printf("Item\t\t\tPrice\tQuantity\tCode \n");
-    printf("Cinnamon rolls\t\t%.2f\t%d\t\t%c \n", price1, Quatites_available1, Selection_code1);
-    printf("Red velvet Cake\t\t%.2f\t%d\t\t%c \n", price2, Quatites_available2, Selection_code2);
-    printf("Chocomilk\t\t%.2f\t%d\t\t%c \n", price3, Quatites_available3, Selection_code3);
+    printf("Cinnamon rolls\t\t$%.2f\t%d\t\t%c \n", price1, Quatites_available1, Selection_code1);
+    printf("Red velvet Cake\t\t$%.2f\t%d\t\t%c \n", price2, Quatites_available2, Selection_code2);
+    printf("Chocomilk\t\t$%.2f\t%d\t\t%c \n", price3, Quatites_available3, Selection_code3);
 }
 
