b427c17f46084e6118c2016c044da0a321609d30
clopezmar4-ux <clopezmar4@gmail.com>
2026-04-24 14:44:25 +0000

    hello

--- hello.c
+++ hello.c
@@ -2,5 +2,12 @@
 
 int main(void)
 {
-    printf("Hello Carol! \n");
+    int Name[50];
+    printf("Enter your name: ");
+    scanf("%s", &Name);
+    printf("Hello %s! \n", Name);
+    int favoriteColor[50];
+    printf("Enter favorite color: ");
+    scanf("%s", &favoriteColor);
+    printf("Your favorite color is: %s \n", favoriteColor);
 }
