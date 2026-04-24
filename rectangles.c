93dec67388a71047496e1338af34b9a5e0715020
clopezmar4-ux <clopezmar4@gmail.com>
2026-04-24 17:12:25 +0000

    rectangles

--- rectangles.c
+++ rectangles.c
@@ -2,7 +2,15 @@
 int main(void){
     int length;
     int width;
+
     printf("Enter the length: ");
     scanf("%d", &length);
     printf("Enter the width: ");
+    scanf("%d", &width);
+
+    int area = length*width;
+    int perimeter = 2*(length+width);
+    printf("Area: %d \n", area);
+     printf("Perimeter: %d \n", perimeter);
+
 }
